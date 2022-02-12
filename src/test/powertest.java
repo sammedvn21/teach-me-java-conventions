e60faf0408f74436c609934e5b69908d7b10f9f2
sammedvn21 <68095659+sammedvn21@users.noreply.github.com>
2022-02-12 11:16:07 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -3,24 +3,24 @@ package powerpackage;
 
 import org.junit.Test;
 
-public class powertest {
+public class PowerTest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void onePowerOne() {
+        assert FindPower.calculatePower(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void twoPowerOne() {
+        assert FindPower.calculatePower(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void twoPowerTwo() {
+        assert FindPower.calculatePower(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void threePowerTwo() {
+        assert FindPower.calculatePower(3, 2) == 3*3;
     }
 }
